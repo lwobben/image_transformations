9e262d00239457074ad1af37ca063e01fa6390b3
lwobben <liana_d_w@hotmail.com>
2023-10-15 12:55:29 +0200

    feat(timelapse): setup

---
+++ timelapse/main.py
@@ -0,0 +1,18 @@
+import cv2
+import os
+import yaml
+
+with open("settings.yaml") as s:
+    config = yaml.safe_load(s)
+
+fourcc = cv2.VideoWriter_fourcc('m', 'p', '4', 'v')
+writer = cv2.VideoWriter(config["path_output"], fourcc, config["speed"], config["size"])
+
+input_folder = config["folder_input"]
+images = [img for img in os.listdir(input_folder) if img.endswith(".jpeg")]
+for image in images:
+    print(image)
+    writer.write(cv2.imread(os.path.join(input_folder, image)))
+
+cv2.destroyAllWindows()
+writer.release()
