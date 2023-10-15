6ff6561b2a6aadbfff443696fb9c4a5f00345412
lwobben <liana_d_w@hotmail.com>
2023-10-15 13:27:15 +0200

    feat(timelapse): iamge_format in settings

--- timelapse/main.py
+++ timelapse/main.py
@@ -9,7 +9,7 @@ fourcc = cv2.VideoWriter_fourcc('m', 'p', '4', 'v')
 writer = cv2.VideoWriter(config["path_output"], fourcc, config["speed"], config["size"])
 
 input_folder = config["folder_input"]
-images = [img for img in os.listdir(input_folder) if img.endswith(".jpeg")]
+images = [img for img in os.listdir(input_folder) if img.endswith(f".{config['image_format']}")]
 for image in images:
     print(image)
     writer.write(cv2.imread(os.path.join(input_folder, image)))
